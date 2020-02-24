e46facd5a71c885046277a1ccff880139b701a17
name <email>
2020-02-24 17:31:25 +0300

    Make ProviderClient public

--- Sources/ProviderSDK/ProviderClient.swift
+++ Sources/ProviderSDK/ProviderClient.swift
@@ -1,17 +1,17 @@
 import Foundation
 
-final class ProviderClient: Codable {
+public final class ProviderClient: Codable {
     
-    var id: UUID?
-    var hostName: String
-    var userName: String
-    var osType: String
-    var osVersion: String
-    var kernelType: String
-    var kernelVersion: String
-    var state: String
-    var cpuUsage: Double?
-    var freeRAM: Int?
+    public var id: UUID?
+    public var hostName: String
+    public var userName: String
+    public var osType: String
+    public var osVersion: String
+    public var kernelType: String
+    public var kernelVersion: String
+    public var state: String
+    public var cpuUsage: Double?
+    public var freeRAM: Int?
     
     init(id: UUID?, hostName: String, userName: String, osType: String, osVersion: String, kernelType: String, kernelVersion: String, state: String, cpuUsage: Double?, freeRAM: Int?) {
         self.id = id
